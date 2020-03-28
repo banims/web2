a4c589dc5059d583419ed3a35e8eade483eec012
banims <banims1@gmail.com>
2020-03-28 08:02:07 +0700

    e-learning

--- latihan03/03latihan1.php
+++ latihan03/03latihan1.php
@@ -7,28 +7,28 @@
 Color:<input type="text" name="ball"><br/>
 <input type="submit" value="Check">
 </form>
-<?
-$ball = $_POST["color"];
-//Coba ubah ke "yellow", "blue", "green", "purple", atau warna lain
-if ($ball == "red") {
-$redbox = $ball;
-} elseif ($ball == "yellow") {
-$yellowbox = $ball;
-} elseif ($ball == "blue") {
-$bluebox = $ball;
-} elseif ($ball == "green") {
-$greenbox = $ball;
-} elseif ($ball == "purple") {
-$purplebox = $ball;
+<?php
+$ball = 'yellow';
+ //Coba ubah ke "yellow", "blue", "green", "purple", atau warna lain
+if ($ball == 'red') {
+ $redbox = $ball;
+ echo "red box : $redbox <br>\n";
+} elseif ($ball == 'yellow') {
+ $yellowbox = $ball;
+ echo "yellow box : $yellowbox <br>\n";
+} elseif ($ball == 'blue') {
+ $bluebox = $ball;
+ echo "blue box : $bluebox <br>\n";
+} elseif ($ball == 'green') {
+ $greenbox = $ball;
+ echo "green box : $greenbox <br>\n";
+} elseif ($ball == 'purple') {
+ $purplebox = $ball;
+ echo "purple box : $purplebox <br>\n";
 } else {
-$colorlessbox = $ball;
+ $colorlessbox = $ball;
+ echo "colorless box : $colorlessbox <br>\n";
 }
-echo "red box: $redbox<br>";
-echo "yellow box: $yellowbox<br>";
-echo "blue box: $bluebox<br>";
-echo "green box: $greenbox<br>";
-echo "purple box: $purplebox<br>";
-echo "colorless box: $colorlessbox<br>";
 ?>
 </body>
 </html>
