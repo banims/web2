f1bb07e31895ddef3b7e2d672dba9d31a4fa5395
banims <banims1@gmail.com>
2020-05-16 01:14:18 +0700

    e-learning

--- latihan11/11latihan1-4/11latihan04.php
+++ latihan11/11latihan1-4/11latihan04.php
@@ -1,17 +1,13 @@
 <?php
-$mysqli = mysqli_connect("localhost:3308","root",""); //koneksi
-mysqli_select_db($mysqli,"lat_dbase"); // mengaktifkan database
-//membuat tabel
-$sql = "CREATE TABLE tbl_mhs
-(
-mhsID int NOT NULL AUTO_INCREMENT,
-PRIMARY KEY(mhsID), 
-FirstName varchar(15),
-LastName varchar(15),
-Age int
-)";
-mysqli_query($mysqli,$sql);
-// input data
-$input=mysqli_query($mysqli,"insert into tbl_mhs(FirstName,LastName,Age)
-values('Anjar','Prabowo',25)");
-?>
+    $con = mysqli_connect("localhost:3308","root","");
+    if (!$con)
+    {
+        die('Could not connect: ' . mysqli_error($con));
+    }
+    mysqli_select_db($con, "lat_dbase");
+    mysqli_query($con, "INSERT INTO tbl_mhs (FirstName, LastName, Age)
+    VALUES ('Karina', 'Suwandi', '29')");
+    mysqli_query($con, "INSERT INTO tbl_mhs (FirstName, LastName, Age)
+    VALUES ('Glenn', 'Gandari', '32')");
+    mysqli_close($con);
+?> 
